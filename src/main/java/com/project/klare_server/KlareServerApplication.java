171398c680d46cce64e621479b0531631cf703d9
joeboy77 <acheampongjoseph470@gmail.com>
2026-06-18 10:44:04 +0000

    feat: project initilization

---
+++ src/main/java/com/project/klare_server/KlareServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.klare_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlareServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlareServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/klare_server/KlareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.klare_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlareServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
